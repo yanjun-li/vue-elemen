06fdc5eba6541055a00d5f7771407d43bf627171
yanjun-li <475509508@qq.com>
2017-10-26 18:05:52 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <aside class="sider">
+      <sider></sider>
+    </aside>
+    <main></main>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+import Sider from './components/Sider'
+export default {
+  name: 'app',
+  components: {
+    'sider': Sider
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: '微软雅黑','Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+</style>
---
+++ src/components/Sider.vue
@@ -0,0 +1,34 @@
+<template>
+  <aside class="sider">
+    <el-form :label-position="labelPosition" label-width="80px" :model="formLabelAlign">
+      <el-form-item label="供水区域">
+        <el-input v-model="formLabelAlign.name"></el-input>
+      </el-form-item>
+      <el-form-item label="供水类型">
+        <el-input v-model="formLabelAlign.region"></el-input>
+      </el-form-item>
+      <el-form-item label="活动形式">
+        <el-input v-model="formLabelAlign.type"></el-input>
+      </el-form-item>
+    </el-form>
+  </aside>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        labelPosition: 'right',
+        formLabelAlign: {
+          name: '',
+          region: '',
+          type: ''
+        }
+      }
+    }
+  }
+</script>
+
+<style>
+
+</style>
